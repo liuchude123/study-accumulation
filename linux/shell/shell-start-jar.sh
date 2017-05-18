8a8de9ee348e15f5b1781a598a063b4634e6c3f4
liuchude123 <878119318@qq.com>
2017-05-18 14:30:34 +0800

    Update shell-start-jar.sh

--- linux/shell/shell-start-jar.sh
+++ linux/shell/shell-start-jar.sh
@@ -104,35 +104,43 @@ function run()
 			echo -e "\033[40;32;1mthe $jarname process does not run!\033[0m"
 		fi
 
-		echo -e "\033[40;35;1mStep 3: Start running your jar!\033[0m"
-		echo -e "\033[40;34;1mstart running $jarname ...\033[0m "
-
-		# get file path
-		path=$(getFilePath $jarname)
-
-		# run jar
-		runJar $jarname $path
-
-		# get pid
-		pid=$(getPid $jarname)
-		if [ "$pid" != "" ]
-			then
-			echo -e "\033[40;32;1mrunning $jarname success $pid ...\033[0m"
+		echo -e "\033[40;35;1mStep 3: Start running your jar?\033[0m"
+		read -p "yes or no? " yesorno
+		# tollower case
+		start=$(toLower $yesorno)
+		if [ "$start" == "yes" ] || [ "$start" == "y" ]
+			then	
+			echo -e "\033[40;34;1mstart running $jarname ...\033[0m "
 			
-			# view memory?
-			echo -e "\033[40;35;1mStep 4: View memory usage information\033[0m"
-			read -p "yes or no? " yesorno
-			# tollower case
-			yn=$(toLower $yesorno)
-			if [ "$yn" == "yes" ] || [ "$yn" == "y" ]
+			# get file path
+			path=$(getFilePath $jarname)
+			# run jar
+			runJar $jarname $path
+
+			# get pid
+			pid=$(getPid $jarname)
+			if [ "$pid" != "" ]
 				then
-					top -p $pid
+				echo -e "\033[40;32;1mrunning $jarname success $pid ...\033[0m"
+				
+				# view memory?
+				echo -e "\033[40;35;1mStep 4: View memory usage information?\033[0m"
+				read -p "yes or no? " yesorno
+				# tollower case
+				yn=$(toLower $yesorno)
+				if [ "$yn" == "yes" ] || [ "$yn" == "y" ]
+					then
+						top -p $pid
+				else
+					echo -e "\033[40;34;1mexit!\033[0m"
+					exit
+				fi
 			else
-				echo -e "\033[40;34;1mexit!\033[0m"
-				exit
+				echo -e "\033[40;31;1mrunning $pname failure ...\033[0m"
 			fi
 		else
-			echo -e "\033[40;31;1mrunning $pname failure ...\033[0m"
+			echo -e "\033[40;34;1mexit!\033[0m"
+			exit
 		fi
 	fi
 }
@@ -140,6 +148,7 @@ function run()
 run
 
 exit
+
 
 
 
